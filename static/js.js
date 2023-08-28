ff1d65de7556ae29a4174a7013aa09acc79f9ff9
wood <wood@czl.net>
2023-08-29 00:33:44 +0800

    优化

--- static/js.js
+++ static/js.js
@@ -1,15 +1,8 @@
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
